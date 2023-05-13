e0b1f534e4e7b4ffae4f570f32e7db7bb42ace92
tobi <tobiaszapata99@gmail.com>
2023-05-12 21:10:31 -0300

    Actions y reducer de allEjercicios/ detail/ clearDetail

--- client/src/redux/Actions/index.js
+++ client/src/redux/Actions/index.js
@@ -1,18 +1,40 @@
 import axios from "axios";
 
-
-
-
-
 /////ejemplo
-export const GET_RUTINAS = "GET_RUTINAS"
+export const GET_RUTINAS = "GET_RUTINAS";
+export const GET_EJERCICIOS = "GET_EJERCICIOS";
+export const GET_EJERCICIOS_ID = "GET_EJERCICIOS_ID";
+export const CLEAR_DETAIL = "CLEAR_DETAIL";
 
 export function getAllRutinas() {
   return async function (dispatch) {
-    var response = await axios ("http://localhost:3001/rutinas")
-    return dispatch ({
+    var response = await axios("http://localhost:3001/rutinas");
+    return dispatch({
       type: GET_RUTINAS,
-      payload: response.data 
-    })
-  }
+      payload: response.data,
+    });
+  };
 }
+
+export const getEjercicios = () => {
+  return async (dispatch) => {
+    const response = await axios.get(
+      "henry-gym-production.up.railway.app/ejercicios"
+    );
+    dispatch({
+      type: GET_EJERCICIOS,
+      payload: response.data,
+    });
+  };
+};
+
+export const searchById = (id) => {
+  return async (dispatch) => {
+    const response = await axios.get(
+      `henry-gym-production.up.railway.app/ejercicios/${id}`
+    );
+    dispatch({ type: GET_EJERCICIOS_ID, payload: response.data });
+  };
+};
+
+export const clearDetail = () => ({ type: CLEAR_DETAIL });
