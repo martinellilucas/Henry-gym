d56f5aa842881b8f9cd3403855c692ca9f407ed5
Lucas Martinelli <martinellilucas8@gmail.com>
2023-05-13 02:06:54 -0300

    controler terminado prueba 2

--- api/src/db.js
+++ api/src/db.js
@@ -4,17 +4,17 @@ const fs = require("fs");
 const path = require("path");
 const { DB_URL, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 
-/* const sequelize = new Sequelize(DB_URL, {
+const sequelize = new Sequelize(DB_URL, {
   logging: false,
   native: false,
-}); */
-const sequelize = new Sequelize(
+});
+/* const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/henrygym`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
-);
+); */
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
