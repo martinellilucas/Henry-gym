4a319edd9db83a853be124ced6d1cb083ec031b6
Lucas Martinelli <martinellilucas8@gmail.com>
2023-05-13 01:24:56 -0300

    getRutinas hecho, prueba 1

--- api/src/controllers/getRutinas.js
+++ api/src/controllers/getRutinas.js
@@ -1,13 +1,17 @@
-const Rutinas = require('../../Rutinas.json')
+const rutinas = require("../../Rutinas.js");
+const { Rutina } = require("../db");
 
+const getRutinas = async () => {
+  const consult = await Rutina.findAll();
+  if (!consult.length) {
+    rutinas.map(async (rutina) => {
+      await Rutina.create({
+        difficulty: rutina.difficulty,
+        grupoMuscular: [...rutina.grupoMuscular],
+      });
+    });
+  }
+  return await Rutina.findAll();
+};
 
-const getRutinas = (Rutinas) => {
-
-return Rutinas
-
-}
-
-
-module.exports = {
-    getRutinas
-}
+module.exports = getRutinas;
