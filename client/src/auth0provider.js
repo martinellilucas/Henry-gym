7c3a90cf21ec18f5cbfdbc852b903598e9d97e28
tobi <tobiaszapata99@gmail.com>
2023-05-17 19:44:36 -0300

    a

--- client/src/auth0provider.js
+++ client/src/auth0provider.js
@@ -6,22 +6,11 @@ const DOMAIN = "dev-oa6kftjco4pbuzjl.us.auth0.com";
 const CLIENT_ID = "6T6Kos97gZ7SHcH6Gf1Fxv6Uu14OK5qP";
 
 const Auth0ProviderWithHistory = ({ children }) => {
-  const history = useNavigate();
-
-  const onRedirectCallback = (appState) => {
-    history.push(appState?.returnTo || window.location.pathname);
-  };
-
-  const onExecutePostLogin = async (event, api) => {
-    api.redirect.sendUserTo("https://henry-gym-pf.onrender.com/home");
-  };
-
   return (
     <Auth0Provider
       domain={DOMAIN}
       clientId={CLIENT_ID}
       redirectUri={window.location.origin}
-      onExecutePostLogin={onExecutePostLogin}
     >
       {children}
     </Auth0Provider>
