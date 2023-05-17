008f4313959ca47520c90d85a6841b7bdc3b1466
tobi <tobiaszapata99@gmail.com>
2023-05-17 17:23:34 -0300

    pruebas

--- client/src/auth0provider.js
+++ client/src/auth0provider.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 const DOMAIN = "dev-7nbk3insf1dr67c3.us.auth0.com";
 const CLIENT_ID = "5GMWPIA9jVbH6tuVHd3lpPN4oTVILKdn";
 
 const Auth0ProviderWithHistory = ({ children }) => {
-  const history = useNavigate();
+  const history = useHistory();
 
-  const onRedirectCallback = (appState) => {
-    history.push(appState?.returnTo || window.location.pathname);
+  const onRedirectCallback = () => {
+    history.push("/home");
   };
 
   return (
