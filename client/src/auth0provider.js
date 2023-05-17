78ac3d7d0c2a4399121c06ff232f3f697b58b0e6
tobi <tobiaszapata99@gmail.com>
2023-05-17 20:15:21 -0300

    a

--- client/src/auth0provider.js
+++ client/src/auth0provider.js
@@ -6,18 +6,11 @@ const DOMAIN = "dev-oa6kftjco4pbuzjl.us.auth0.com";
 const CLIENT_ID = "6T6Kos97gZ7SHcH6Gf1Fxv6Uu14OK5qP";
 
 const Auth0ProviderWithHistory = ({ children }) => {
-  const history = useNavigate();
-
-  const onRedirectCallback = (appState) => {
-    history.push(appState?.returnTo || window.location.pathname);
-  };
-
   return (
     <Auth0Provider
       domain={DOMAIN}
       clientId={CLIENT_ID}
       redirectUri={window.location.origin}
-      onRedirectCallback={onRedirectCallback}
     >
       {children}
     </Auth0Provider>
