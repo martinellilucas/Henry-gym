0ddb49409e134628ec372d27c5a8f39ad83fd21c
tobi <tobiaszapata99@gmail.com>
2023-05-17 19:08:47 -0300

    cambios

--- client/src/App.js
+++ client/src/App.js
@@ -10,27 +10,24 @@ import SmallCentered from "../src/components/Footer/footer";
 import Pagination from "./components/PaginationEjercicios/Pagination";
 import PaginationRutinas from "./components/PaginationRutinas/PaginationRutinas";
 import PostRutina from "./components/CrearRutina/CrearRutina";
-import Auth0ProviderWithHistory from "./auth0provider";
 
 function App() {
   const { pathname } = useLocation();
   return (
-    <Auth0ProviderWithHistory>
-      <div>
-        {pathname !== "/" && <Nav />}
-        <Routes>
-          <Route path="*" element={<Error />} />
-          <Route path="/" element={<Landing />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/detail/:id" element={<Detail />} />
-          <Route path="/rutinas" element={<PaginationRutinas />} />
-          <Route path="/ejercicios" element={<Pagination />} />
-          <Route path="/form" element={<PostRutina />} />
-        </Routes>
-        {pathname !== "/" && <SmallCentered />}
-      </div>
-    </Auth0ProviderWithHistory>
+    <div>
+      {pathname !== "/" && <Nav />}
+      <Routes>
+        <Route path="*" element={<Error />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/rutinas" element={<PaginationRutinas />} />
+        <Route path="/ejercicios" element={<Pagination />} />
+        <Route path="/form" element={<PostRutina />} />
+      </Routes>
+      {pathname !== "/" && <SmallCentered />}
+    </div>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -7,16 +7,25 @@ import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
 import store from "../src/redux/Store/store";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </BrowserRouter>
-  </Provider>
+  <Auth0Provider
+    domain="dev-oa6kftjco4pbuzjl.us.auth0.com"
+    clientId="6T6Kos97gZ7SHcH6Gf1Fxv6Uu14OK5qP"
+    authorizationParams={{
+      redirect_uri: "/home",
+    }}
+  >
+    <Provider store={store}>
+      <BrowserRouter>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </BrowserRouter>
+    </Provider>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
