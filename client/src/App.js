715b560debe13babd3f5179c8a3d86022e0e7d05
tobi <tobiaszapata99@gmail.com>
2023-05-18 16:38:58 -0300

    a

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ function App() {
       <div>
         {pathname !== "/" && <Nav />}
         <Routes>
+          <Route path="*" element={<Error />} />
           <Route path="/" element={<Landing />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -26,7 +27,6 @@ function App() {
           <Route path="/rutinas" element={<PaginationRutinas />} />
           <Route path="/ejercicios" element={<Pagination />} />
           <Route path="/form" element={<PostRutina />} />
-          <Route element={<Error />} />
         </Routes>
         {pathname !== "/" && <SmallCentered />}
       </div>
