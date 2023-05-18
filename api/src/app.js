2f938f569e4a5a36142f09e1054d330a741ae790
tobi <tobiaszapata99@gmail.com>
2023-05-18 14:24:59 -0300

    a

--- api/src/app.js
+++ api/src/app.js
@@ -14,7 +14,7 @@ server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); //cambiar cuando este deployado por la url del client
+  res.header("Access-Control-Allow-Origin", "https://henry-gym.onrender.com"); //cambiar cuando este deployado por la url del client
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
