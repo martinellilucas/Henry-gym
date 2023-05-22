d0fb25927aea855ba44e26681fab7bcc7a49ec5a
Lucas Martinelli <martinellilucas8@gmail.com>
2023-05-21 22:15:56 -0300

    correccion en getbyid

--- api/src/routes/routeCliente.js
+++ api/src/routes/routeCliente.js
@@ -2,10 +2,13 @@ const { Router } = require("express");
 const handlerGetClientes = require("../handlers/handlerGetClientes");
 const handlerPostCliente = require("../handlers/handlerPostCliente");
 const handlerPutCliente = require("../handlers/handlerPutCliente");
+const handlerIDCliente = require("../handlers/handlerIDCliente");
 const routeCliente = Router();
 
 routeCliente.get("/", handlerGetClientes);
+routeCliente.get("/:id", handlerIDCliente);
 routeCliente.post("/", handlerPostCliente);
+
 routeCliente.put("/:email", handlerPutCliente);
 
 module.exports = routeCliente;
