00e94ca8c052aa3230dcec337c72833689d10088
Lucas Martinelli <martinellilucas8@gmail.com>
2023-05-23 17:01:37 -0300

    error corregido

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -13,7 +13,7 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </Provider>,
-document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
