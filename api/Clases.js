cbf5f3240b803bf6688673e9e4f6fd2c37410db8
tobi <tobiaszapata99@gmail.com>
2023-05-24 16:06:02 -0300

    a

---
+++ api/Clases.js
@@ -0,0 +1,94 @@
+const clases = [
+  {
+    nombre: "crossfit",
+    dias: ["Lunes", "Martes", "Miércoles"],
+    horario: "10:00",
+  },
+  {
+    nombre: "crossfit",
+    dias: ["Miércoles", "Jueves", "Viernes"],
+    horario: "20:00",
+  },
+  {
+    nombre: "crossfit",
+    dias: ["Sábado"],
+    horario: "10:00",
+  },
+  {
+    nombre: "functional",
+    dias: ["Lunes", "Miércoles", "Viernes"],
+    horario: "19:30",
+  },
+  {
+    nombre: "functional",
+    dias: ["Martes", "Jueves"],
+    horario: "17:00",
+  },
+  {
+    nombre: "functional",
+    dias: ["Sábado"],
+    horario: "11:30",
+  },
+  {
+    nombre: "yoga",
+    dias: ["Martes", "Jueves"],
+    horario: "9:00",
+  },
+  {
+    nombre: "yoga",
+    dias: ["Miércoles", "Viernes"],
+    horario: "17:30",
+  },
+  {
+    nombre: "yoga",
+    dias: ["Sábado"],
+    horario: "9:30",
+  },
+  {
+    nombre: "zumba",
+    dias: ["Lunes", "Miércoles", "Viernes"],
+    horario: "18:30",
+  },
+  {
+    nombre: "zumba",
+    dias: ["Martes", "Jueves"],
+    horario: "10:30",
+  },
+  {
+    nombre: "zumba",
+    dias: ["Sábado"],
+    horario: "10:30",
+  },
+  {
+    nombre: "pilates",
+    dias: ["Lunes", "Miércoles", "Viernes"],
+    horario: "10:00",
+  },
+  {
+    nombre: "pilates",
+    dias: ["Martes", "Jueves"],
+    horario: "19:00",
+  },
+  {
+    nombre: "pilates",
+    dias: ["Sábado"],
+    horario: "11:00",
+  },
+  {
+    nombre: "musculacion",
+    dias: ["Lunes", "Martes", "Miércoles"],
+    horario: "8:30",
+  },
+  {
+    nombre: "musculacion",
+    dias: ["Miércoles", "Jueves", "Viernes"],
+    horario: "16:00",
+  },
+  {
+    nombre: "musculacion",
+    dias: ["Sábado"],
+    horario: "9:00",
+  },
+];
+
+module.exports = clases;
