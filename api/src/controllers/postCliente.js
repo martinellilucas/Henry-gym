b997d1fd3b7c4fd85c888efe8d00181caa0dd8f2
Lucas Martinelli <martinellilucas8@gmail.com>
2023-05-25 19:42:29 -0300

    post cliente no postea el cliente

--- api/src/controllers/postCliente.js
+++ api/src/controllers/postCliente.js
@@ -1,15 +1,14 @@
 const { Cliente } = require("../db");
 
-const postCliente = async (name,email,email_verified,picture) => {
-
+const postCliente = async ({ name, email, email_verified, picture }) => {
   const newClient = await Cliente.findOrCreate({
-    where : {
-    nombre : name,
-    email : email,
-    emailVerified: email_verified,
-    picture : picture
-    }
-  })
+    where: {
+      nombre: name,
+      email: email,
+      emailVerified: email_verified,
+      picture: picture,
+    },
+  });
 
   return newClient;
 };
