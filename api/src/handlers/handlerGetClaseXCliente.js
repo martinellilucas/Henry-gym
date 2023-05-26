315cbad2737b36fc4940a9cd490d55a5356e0e2c
SebastianGaviriaM <sebastiangaviriamontoya@gmail.com>
2023-05-26 14:47:54 -0500

    Fix getClaseXCliente

--- api/src/handlers/handlerGetClaseXCliente.js
+++ api/src/handlers/handlerGetClaseXCliente.js
@@ -3,7 +3,7 @@ const getClaseXCliente = require("../controllers/getClaseXCliente");
 const handlerGetClaseXCliente = async(req, res) => {
   try {
     const { id } = req.params;
-    const consulta = getClaseXCliente(id);
+    const consulta = await getClaseXCliente(id);
     res.status(200).json(consulta);
   } catch (error) {
     res.status(400).json({ error: error.message });
