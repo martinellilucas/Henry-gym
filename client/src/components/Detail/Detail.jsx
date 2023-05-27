923ee60223ddd596e6d2ac646799d080d7c1255f
Lucas Martinelli <martinellilucas8@gmail.com>
2023-05-27 03:06:01 -0300

    estilo detail

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -22,26 +22,27 @@ const Detail = () => {
   );
   return (
     <div className={style.body}>
+      <h1 className={style.title}>ROUTINE DETAIL</h1>
       <div className={style.container}>
         {rutinaDetail ? (
           <div className={style.card}>
             <div className={style.rutina}>
               <img className={style.img} src={rutinaDetail.imagen} alt="img" />
-              <h1 className={style.title}>
+              <h1 className={style.titleCard}>
                 Difficulty: {rutinaDetail.difficulty}
               </h1>
               <h2 className={style.grupoMuscular}>
-                Grupo Muscular: {rutinaDetail.grupoMuscular}
+                Muscle: {rutinaDetail.grupoMuscular}
               </h2>
             </div>
             <div className={style.ejercicios}>
-              <h1 className={style.title}>Ejercicios</h1>
+              <h1 className={style.titleCard}>Exercises</h1>
               {filtrados.map((e) => {
                 return (
                   <div key={e.id}>
-                    <h1 className={style.title}>{e.name}</h1>
+                    <h1 className={style.subTitle}>{e.name}</h1>
 
-                    <h3 className={style.title}>Instructions </h3>
+                    <h3 className={style.subTitle}>Instructions </h3>
                     <p className={style.instructions}>{e.instructions}</p>
                   </div>
                 );
