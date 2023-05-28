e4c6402ce81db8f64219e632caa00a020e18c6d6
Lucas Martinelli <martinellilucas8@gmail.com>
2023-05-28 19:03:57 -0300

    estilado error mejorado para hacerlo responsive

--- client/src/components/Error/Error.jsx
+++ client/src/components/Error/Error.jsx
@@ -1,18 +1,18 @@
-import { Image } from "@chakra-ui/react";
 import { Box } from "@chakra-ui/react";
-import { Button } from "@chakra-ui/react";
-import imagen from "../../assets/Error.gif";
-import s from "./Error.module.css";
+
+import style from "./Error.module.css";
 import { NavLink } from "react-router-dom";
 
 const Error = () => {
   return (
-    <Box className={s.container}>
-      <NavLink to={"/home"}>
-        <Button className={s.button}>Volver al home</Button>
-      </NavLink>
-
-      <h1 className={s.descripcion}>ERROR 404</h1>
+    <Box className={style.container}>
+      <div className={style.img}></div>
+      <div className={style.descContainer}>
+        <h1 className={style.descripcion}>ERROR 404 PAGE NOT FOUND </h1>
+        <NavLink to={"/home"}>
+          <button className={style.button}>HOME</button>
+        </NavLink>
+      </div>
     </Box>
   );
 };
