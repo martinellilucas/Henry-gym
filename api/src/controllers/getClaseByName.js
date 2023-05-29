02f1ecb181a5b95bfee427dab907f4ed7b87d2eb
Lucas Martinelli <martinellilucas8@gmail.com>
2023-05-29 20:40:56 -0300

    mejorado el search clase

--- api/src/controllers/getClaseByName.js
+++ api/src/controllers/getClaseByName.js
@@ -1,11 +1,14 @@
 const { Clase } = require("../db");
+const { Op } = require("sequelize");
 
 const getClaseByName = async (name) => {
   let consulta;
   if (name.includes("-")) {
     consulta = await Clase.findOne({ where: { id: name } });
   } else {
-    consulta = await Clase.findAll({ where: { nombre: name } });
+    consulta = await Clase.findAll({
+      where: { nombre: { [Op.iLike]: `%${name}` } },
+    });
   }
 
   return consulta;
