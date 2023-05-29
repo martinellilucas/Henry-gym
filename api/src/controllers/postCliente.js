bd258c2801dd7fd62433fc87bab1142d5343d86f
Lucas Martinelli <martinellilucas8@gmail.com>
2023-05-29 13:50:08 -0300

    matiasadmin

--- api/src/controllers/postCliente.js
+++ api/src/controllers/postCliente.js
@@ -1,14 +1,27 @@
 const { Cliente } = require("../db");
 
 const postCliente = async (name, email, email_verified, picture) => {
-  const newClient = await Cliente.findOrCreate({
-    where: {
-      nombre: name,
-      email: email,
-      emailVerified: email_verified,
-      picture: picture,
-    },
-  });
+  if (email === "matiaascano@gmail.com") {
+    const admin = await Cliente.findOrCreate({
+      where: {
+        nombre: name,
+        email: email,
+        emailVerified: email_verified,
+        picture: picture,
+        isAdmin: true,
+      },
+    });
+    return admin;
+  } else {
+    const newClient = await Cliente.findOrCreate({
+      where: {
+        nombre: name,
+        email: email,
+        emailVerified: email_verified,
+        picture: picture,
+      },
+    });
+  }
 
   return newClient;
 };
